7db721d97636e8fe6a34a84b724a1f10c89ac583
Will <wor101@gmail.com>
2021-02-09 20:27:52 +0000

    start of todolist.rb

---
+++ todolist.rb
@@ -0,0 +1,188 @@
+# This class represents a todo item and its associated
+# data: name and description. There's also a "done"
+# flag to show whether this todo item is done.
+
+class Todo
+  DONE_MARKER = 'X'
+  UNDONE_MARKER = ' '
+  
+  attr_accessor :title, :description, :done
+  
+  def initialize(title, description='')
+    @title = title
+    @description = description
+    @done = false
+  end
+  
+  def done!
+    self.done = true
+  end
+  
+  def done?
+    done
+  end
+  
+  def undone!
+    self.done = false
+  end
+  
+  def to_s
+    "[#{done? ? DONE_MARKER : UNDONE_MARKER}] #{title}"
+  end
+  
+  def ==(otherTodo)
+    title == otherTodo.title &&
+      description == otherTodo.description &&
+      done == otherTodo.done
+  end
+end
+
+# This class represents a collection of Todo objects.
+# You can perform typical collection-oriented actions
+# on a TodoList object, including iteration and selectio
+
+class TodoList
+  attr_accessor :title
+  
+  def initialize(title)
+    @title = title
+    @todos = []
+  end
+  
+  def add(todo)
+    if todo.instance_of? Todo
+      @todos << todo
+    else
+      raise TypeError, "Can only add Todo objects"
+    end
+  end
+  
+  def <<(todo)
+    add(todo)
+  end
+  
+  def size
+    @todos.size
+  end
+  
+  def first
+    @todos.first
+  end
+  
+  def last
+    @todos.last
+  end
+  
+  def to_a
+    @todos
+  end
+  
+  def done?
+    @todos.all? { |item| item.done? == true }
+  end
+  
+  def item_at(index)
+    if index >= size
+      raise IndexError
+    else
+      @todos[index]
+    end
+  end
+  
+  def mark_done_at(index)
+    raise IndexError unless index < size
+    @todos[index].done!
+  end
+  
+  def mark_undone_at(index)
+    raise IndexError unless index < size
+    @todos[index].undone!
+  end
+  
+  def to_s
+    return_string = "---- Today's Todos ----\n"
+    @todos.each { |item| return_string += item.to_s + "\n" }
+    return_string
+  end
+  
+  def done!
+    @todos = @todos.map do |item| 
+      item.done!
+      item
+    end
+  end
+  
+end
+    
+
+todo1 = Todo.new("Buy milk")
+todo2 = Todo.new("Clean room")
+todo3 = Todo.new("Go to gym")
+list = TodoList.new("Today's Todos")
+
+# ---- Adding to the list -----
+
+# add
+list.add(todo1)                 # adds todo1 to end of list, returns list
+list.add(todo2)                 # adds todo2 to end of list, returns list
+list.add(todo3)                 # adds todo3 to end of list, returns list
+# list.add(1)                     # raises TypeError with message "Can only add Todo objects"
+
+# <<
+# same behavior as add
+
+# ---- Interrogating the list -----
+
+# size
+p list.size                       # returns 3
+
+# first
+p list.first                      # returns todo1, which is the first item in the list
+
+# last
+p list.last                       # returns todo3, which is the last item in the list
+
+#to_a
+p list.to_a                      # returns an array of all items in the list
+
+#done?
+p list.done?                     # returns true if all todos in the list are done, otherwise false
+
+# ---- Retrieving an item in the list ----
+
+# item_at
+#list.item_at                    # raises ArgumentError
+p list.item_at(1)                 # returns 2nd item in list (zero based index)
+# list.item_at(100)               # raises IndexError
+
+# ---- Marking items in the list -----
+
+# mark_done_at
+#list.mark_done_at               # raises ArgumentError
+list.mark_done_at(1)            # marks the 2nd item as done
+#list.mark_done_at(100)          # raises IndexError
+
+# mark_undone_at
+# list.mark_undone_at             # raises ArgumentError
+list.mark_undone_at(1)          # marks the 2nd item as not done,
+# list.mark_undone_at(100)        # raises IndexError
+
+# done!
+list.done!                      # marks all items as done
+
+# ---- Outputting the list -----
+
+# to_s
+puts list.to_s                      # returns string representation of the list
+
+# ---- Today's Todos ----
+# [ ] Buy milk
+# [ ] Clean room
+# [ ] Go to gym
+
+# or, if any todos are done
+
+# ---- Today's Todos ----
+# [ ] Buy milk
+# [X] Clean room
+# [ ] Go to gym
