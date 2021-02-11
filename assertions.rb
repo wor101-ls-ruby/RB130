4748b713084926731b7998ada17cb627fbb91248
Will <wor101@gmail.com>
2021-02-11 19:49:19 +0000

    adding assertions and testing for equality

--- assertions.rb
+++ assertions.rb
@@ -3,37 +3,41 @@ require 'minitest/autorun'
 require_relative 'car'
 
 class CarTest < MiniTest::Test
+  def setup
+    @car = Car.new
+  end
+  
   # assert(test) test fails unless test is truthy
   def test_car_exists
-    car = Car.new
-    assert(car)
+    assert(@car)
   end
   
   # assert_equal(exp, act) fails unless exp == act
   def test_wheels
-    car = Car.new
-    assert_equal(4, car.wheels)
+    assert_equal(4, @car.wheels)
   end
   
   # assert_nil(obj) fails unless obj is nil
   def test_name_is_nil
-    car = Car.new
-    assert_nil(car.name)
+    assert_nil(@car.name)
+  end
+  
+  def test_raise_initialize_with_arg
+    assert_raises(ArgumentError) do
+      Car.new(name: "Joey")
+    end
   end
   
   # assert_instance_of(cls, obj) fails unless obj is an instance of cls
   def test_instance_of_car
-    car = Car.new
-    assert_instance_of(Car, car)
+    assert_instance_of(Car, @car)
   end
   
   # assert_includes(collection, obj)
   def test_includes_car
-    car = Car.new
     arr = [1, 2, 3]
-    arr << car
+    arr << @car
     
-    assert_includes(arr, car)
+    assert_includes(arr, @car)
   end
-  
 end
